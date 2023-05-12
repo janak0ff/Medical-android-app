25f2413340445cc65e5cd68c899d362dd51f1ca7
sudiplun <sudeeplun@gmail.com>
2023-05-12 13:02:57 +0545

    remove local html

--- app/src/main/assets/local-html/js/myvid.js
+++
@@ -1,9 +0,0 @@
-
-// var video_idname = "1xYLyOb8xxh1QG/Acne";
-// var vdorul = "https://" + video_idname + ".mp4";
-// document.getElementById("acnevdo").setAttribute("href", vdorul);
-// // ------------------------------
-
-
-
-
